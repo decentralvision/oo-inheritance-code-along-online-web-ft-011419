2cfd8bd7e0342921c3afa4051108c455527409cf
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-31 07:01:15 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,9 @@
 class Vehicle
-
+  attr_accessor :wheel_size, :number
+  def initialize(wheel_size, number)
+    @wheel_size, @number = wheel_size, number
+  end
+  def go
+    'vrrrrrrrooom!'
+  end
 end
